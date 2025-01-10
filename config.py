1ec92656820525fecf6d3c72a9698d6c4d2c0d88
devil12820 <152226726+devil12820@users.noreply.github.com>
2025-01-11 02:51:29 +0530

    Update config.py
    Signed-off-by: devil12820 <152226726+devil12820@users.noreply.github.com>

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("20345875"))
+API_HASH = getenv("488d799fb37ac8226d4b40ed6f3cdf6a")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7052627510:AAG_0HhD_SzdrW8usy79y1TCiqf7vXIi6bc")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
